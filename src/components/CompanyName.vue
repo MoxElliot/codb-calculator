f76fc706bed4fdf3d78c8a8256c574e164f6125b
Elliot Erickson <elliot_erickson@protonmail.com>
2023-04-17 21:41:46 -0700

    Company Name component
    created form with label for user input of company name

---
+++ src/components/CompanyName.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+
+//import Company from '../types/Company'
+// interface Company {
+//     // name: string
+//     required: true
+// }
+// const props = defineProps<Company>()
+</script>
+
+<template>
+    <form class="m-2 flex flex-col">
+    <label for="company-name">Company Name:</label>
+    <input class="border" id="company-name" name="company-name" type="text" placeholder="Enter Company Name"/>
+    </form>
+</template>
+
