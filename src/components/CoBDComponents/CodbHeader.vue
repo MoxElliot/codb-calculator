7599355f4fe237be81c53c601556540ed71c3819
Elliot Erickson <elliot_erickson@protonmail.com>
2023-04-20 17:10:55 -0700

    Cleaning up styling

--- src/components/CoBDComponents/CodbHeader.vue
+++ src/components/CoBDComponents/CodbHeader.vue
@@ -1,7 +1,23 @@
-<script setup lang="ts"></script>
-
 <template>
-  <div>Cost of Doing Business Calculator</div>
+  <div>
+    <h2>Cost of Doing Business Calculator</h2>
+    <router-link class="border border-black m-1" to="/company-name-step">
+      Company Name Step
+    </router-link>
+    <router-link class="border border-black m-1" to="/fixed-cost-step">
+      Fixed Cost Step
+    </router-link>
+    <router-link class="border border-black m-1" to="/variable-cost-step">
+      Variable Cost Step
+    </router-link>
+    <router-link class="border border-black m-1" to="/booking-income-step">
+      Booking Income Step
+    </router-link>
+    <router-link class="border border-black m-1" to="/owner-draw-step">
+      Owner's Draw Step
+    </router-link>
+    <router-link class="border border-black m-1" to="/final-report-step">
+      Final Report Step
+    </router-link>
+  </div>
 </template>
-
-<style scoped></style>
