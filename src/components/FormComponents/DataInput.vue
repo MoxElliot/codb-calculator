72c5b762919bef8fdf9eee1987c4d53d57492092
Elliot Erickson <elliot_erickson@protonmail.com>
2023-04-23 12:24:17 -0700

    Changing task to building a reusuable form, will delete this branch

---
+++ src/components/FormComponents/DataInput.vue
@@ -0,0 +1,20 @@
+<script setup lang='ts'>
+    const props = defineProps({
+        label: {
+            type: String,
+            default: '',
+        },
+    })
+</script>
+
+
+<template>
+    <label class="label expense-name-label m-1" for="expense-name">{{ label }}</label>
+        <input
+          :placeholder="label"
+          class="input expense-name-input border border-black m-1"
+          id="expense-name"
+        />
+</template>
+
+
