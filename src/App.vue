774f1bb508b846f838d2f34b496ef709ee4ab969
Elliot Erickson <elliot_erickson@protonmail.com>
2023-04-26 11:58:49 -0700

    Changed App.vue to be in the format suggested by Dan

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+</script>
+
 <template>
-  <div class="app">
-    <router-view />
-  </div>
+  <RouterView />
 </template>
+
+<style></style>
