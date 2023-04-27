705b577cf2d83962c074a890bf6e58975f155f79
Elliot Erickson <elliot_erickson@protonmail.com>
2023-04-27 10:44:24 -0700

    removed console.logs and dummy cost data

--- src/stores/fixedCostStore.ts
+++ src/stores/fixedCostStore.ts
@@ -4,24 +4,12 @@ import type FixedCostState from '../types/FixedCostState'
 
 export const useFixedCostStore = defineStore('fixedCostStore', {
   state: (): FixedCostState => ({
-    fixedCosts: [
-      {
-        id: 1,
-        name: 'Rent',
-        category: 'Overhead',
-        amount: 1000,
-        payPeriod: 'Monthly',
-        total: 12000
-      },
-      { id: 2, name: 'Parking', category: 'Overhead', amount: 80, payPeriod: 'Weekly', total: 4160 }
-    ],
-    totalFixedCosts: 100,
+    fixedCosts: [],
+    totalFixedCosts: 0
   }),
   actions: {
     addFixedCostAction(fixedCost: FixedCostObj) {
-      console.log("in FixedCostStore before", this.fixedCosts)
       this.fixedCosts.push(fixedCost)
-      console.log("in FixedCostStore after", this.fixedCosts)
-    },
+    }
   }
 })
