73747f899a55804294cc919619f54d2e87c0b73b
Elliot Erickson <elliot_erickson@protonmail.com>
2023-04-29 09:54:02 -0700

    Indivdual Fixed Cost calculates based on pay period
    in fixedCostStore -> addFixedCostAction I find an element which eqauls the payPeriod, then set the multiplier to equal the multiplier in the element.

---
+++ src/assets/payPeriodOptionsArray.ts
@@ -0,0 +1,9 @@
+// const payPeriodObject: { weekly: number; bimonthly: number; monthly: number; yearly: number } = {
+//   weekly: 52,
+//   bimonthly: 24,
+//   monthly: 12,
+//   yearly: 1
+// }
+
+const payPeriodOptionsArray: {day:string, multiplier:number}[] = [{day:"Weekly", multiplier: 52}, {day: "Bi-monthly", multiplier: 24}, {day: "Monthly", multiplier: 12}, {day: "Yearly", multiplier: 1}] 
+export default payPeriodOptionsArray
--- src/stores/fixedCostStore.ts
+++ src/stores/fixedCostStore.ts
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia'
 import type FixedCostObj from '../types/FixedCostObj'
 import type FixedCostState from '../types/FixedCostState'
-import payPeriodObject from '@/assets/payPeriodObject'
+import payPeriodOptionsArray from '@/assets/payPeriodOptionsArray'
 
 export const useFixedCostStore = defineStore('fixedCostStore', {
   state: (): FixedCostState => ({
@@ -12,16 +12,34 @@ export const useFixedCostStore = defineStore('fixedCostStore', {
     totalFixedCostAction() {
       let costArr: number[] = []
       Object.entries(this.fixedCosts).forEach(([key, val]) => {
-        costArr.push(Number(val.amount)) //Unsure why amount is a string, my data-input is typed to number
+        costArr.push(Number(val.individualTotal)) //Unsure why amount is a string, my data-input is typed to number
       })
       this.totalFixedCosts = costArr.reduce((a,b) => a + b)
     },
     addFixedCostAction(fixedCost: FixedCostObj) {
-      console.log(payPeriodObject.weekly)
-      console.log(fixedCost.payPeriod)
-      const period = fixedCost.payPeriod
-      console.log(period)
-      fixedCost.individualTotal = fixedCost.amount * 4
+      // const period = fixedCost.payPeriod
+      // let periodMultiplier = 0
+      console.log("in store, fixedCostAction payPeridObj", payPeriodOptionsArray)
+      // console.log("in store, fixedCostAction payPeridObj.weekly", payPeriodOptionsArray[0])
+      // const periodMultiplierFinder = () => {
+       
+      //   for(let i = 0; i++; i < 4) {
+      //     payPeriodOptionsArray[i][0] === fixedCost.payPeriod
+      //     console.log("in")
+      //     ? periodMultiplier = payPeriodOptionsArray[i][1] 
+      //     : "error"
+      //   }
+      //   return periodMultiplier
+      // }
+      // console.log("in store, periodMultiplier", periodMultiplier)
+      // console.log("in store, fixedCostAction fixedCost", fixedCost)
+      // console.log("in store, fixedCostAction fixedCost.payPeriod",fixedCost.payPeriod)
+      // console.log("in store, does it equal", payPeriodOptionsArray[0][0] === fixedCost.payPeriod)
+      const payPeriodMultiplierElement = payPeriodOptionsArray.find(ele => ele.day === fixedCost.payPeriod)
+      const payPeriodMultiplier: any = payPeriodMultiplierElement?.multiplier
+      console.log( "in store, periodMultiplier", payPeriodMultiplier)
+
+      fixedCost.individualTotal = fixedCost.amount * payPeriodMultiplier
       this.fixedCosts.push(fixedCost)
       this.totalFixedCostAction()
     },
