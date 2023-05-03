76363f291c0108da1d47b56ee34ea2056cea387d
Mox Elliot <45051420+MoxElliot@users.noreply.github.com>
2023-05-03 13:17:55 -0700

    component reusable button (#24)
    * Properly configured Tailwind
    
    used https://tailwindcss.com/docs/guides/vite, imported index.css into main.ts
    
    * Created FormButton
    
    Created a simple form-button component and reused throught code (for add, next, previous buttons)

--- src/components/CoBDComponents/CodbFooter.vue
+++ src/components/CoBDComponents/CodbFooter.vue
@@ -1,49 +1,25 @@
 <script setup lang="ts">
 import { useStepStore } from '../../stores/stepStore'
+import FormButton from '../FormComponents/FormButton.vue'
 
-// const handleSubmit = () => {
-//   stepStore.forwardStepAction()
-// }
 const stepStore = useStepStore()
 </script>
 
 <template>
   <div>
-    <!-- v-if="stepStore.stepName !== '/user-email-step'" -->
     <router-link :to="stepStore.stepName">
-      <button
-        class="border border-black w-32 rounded-full m-1 bg-cyan-500"
+      <form-button 
+        label="Previous"
         @click="stepStore.backStepAction()"
-      >
-        Previous
-      </button>
+        class="w-32"
+      />
     </router-link>
     <router-link :to="stepStore.stepName">
-      <button
-        class="border border-black w-32 rounded-full m-1 bg-cyan-500"
+      <form-button 
+        label="Next"
         @click="stepStore.forwardStepAction()"
-      >
-        Next
-      </button>
+        class="w-32"
+      />
     </router-link>
   </div>
-  <!-- <div v-if="stepStore.stepName === '/user-email-step'">
-    <router-link :to="stepStore.stepName">
-      <button
-        class="border border-black w-32 rounded-full m-1 bg-cyan-500"
-        @click="stepStore.backStepAction()"
-      >
-        Cancel
-      </button>
-    </router-link>
-    <router-link :to="stepStore.stepName">
-     
-      <button
-        class="border border-black w-32 rounded-full m-1 bg-cyan-500"
-        @click="stepStore.forwardStepAction()"
-      >
-        Submit
-      </button>
-    </router-link>
-  </div> -->
 </template>
