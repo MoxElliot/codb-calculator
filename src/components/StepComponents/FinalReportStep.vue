0b31261ea99fb74f1446dbb45a750603143236a9
Elliot Erickson <elliot_erickson@protonmail.com>
2023-05-04 11:12:15 -0700

    end of day, refactoring getter/actions to work with more general actions

--- src/components/StepComponents/FinalReportStep.vue
+++ src/components/StepComponents/FinalReportStep.vue
@@ -12,18 +12,22 @@ const reportStore = useReportStore()
   </div>
   <div class="m-3">
     <p>Your Yearly Variable Costs</p>
-    <p>${{ reportStore.averageYearlyVariableCosts }}</p>
+    <!-- <p>${{ reportStore.averageYearlyVariableCosts }}</p> -->
+    <p> {{ reportStore.averageYearly(reportStore.totalVariableCosts) }}</p>
     <p>Your Yearly Fixed Costs</p>
-    <p>${{ reportStore.averageYearlyFixedCosts }}</p>
+    <!-- <p>${{ reportStore.averageYearlyFixedCosts }}</p> -->
+    <p> {{ reportStore.averageYearly(reportStore.totalFixedCosts) }}</p>
   </div>
   <div class="m-3">
     <div>
       <p>Your Yearly Savings</p>
-      <p>${{ reportStore.averageYearlySavings }}</p>
+      <!-- <p>${{ reportStore.averageYearlySavings }}</p> -->
+      <p> {{ reportStore.averageYearly(reportStore.savingsPerMonth) }}</p>
     </div>
     <div>
       <p>Owners Draw (Yearly Salary)</p>
-      <p>${{ reportStore.averageYearlyPay }}</p>
+      <!-- <p>${{ reportStore.averageYearlyPay }}</p> -->
+      <p> {{ reportStore.averageYearly(reportStore.payPerMonth) }}</p>
     </div>
     <div>
       <p>{{ reportStore.companyName }}'s Cost of Doing Business'</p>
@@ -33,15 +37,18 @@ const reportStore = useReportStore()
   <div class="m-3">
     <div>
       <p>Number of Bookings to Break Even</p>
-      <p>{{ reportStore.bookingsToBreakEven }}</p>
+      <!-- <p>{{ reportStore.bookingsToBreakEven }}</p> -->
+      
     </div>
     <div>
       <p>Average Yearly Bookings</p>
-      <p>{{ reportStore.averageYearlyBookings }}</p>
+      <!-- <p>{{ reportStore.averageYearlyBookings }}</p> -->
+      <p> {{ reportStore.averageYearly(reportStore.bookingsPerMonth) }}</p>
     </div>
     <div>
       <p>Average Hours Worked Per Year</p>
-      <p>{{ reportStore.hoursWorkedYearly }}</p>
+      <!-- <p>{{ reportStore.hoursWorkedYearly }}</p> -->
+      <p> {{ reportStore.averageYearly(reportStore.hoursAveragePerBooking * reportStore.bookingsPerMonth) }}</p>
     </div>
     <div>
       <p>Your Hourly Rate</p>
@@ -50,6 +57,7 @@ const reportStore = useReportStore()
     <div>
       <p>Your Average Yearly Income</p>
       <p>${{ reportStore.averageYearlyIncome }}</p>
+      <p> {{ reportStore.averageYearly(reportStore.totalFixedCosts) }}</p>
     </div>
   </div>
 </template>
--- src/stores/reportStore.ts
+++ src/stores/reportStore.ts
@@ -20,49 +20,93 @@ export const useReportStore = defineStore('reportStore', {
     userEmail: ''
   }),
   getters: {
-    averageYearlyPay(): number {
-      return this.payPerMonth * 12
-    },
-    averageYearlySavings(): number {
-      return this.savingsPerMonth * 12
-    },
-    averageYearlyFixedCosts(): number {
-      return this.totalFixedCosts * 12
-    },
-    averageYearlyVariableCosts(): number {
-      return this.totalVariableCosts * 12
-    },
-    costOfDoingBusiness(): number {
-      return (
-        this.averageYearlyFixedCosts +
-        this.averageYearlyVariableCosts +
-        this.averageYearlySavings +
-        this.averageYearlyPay
-      )
-    },
-    bookingsToBreakEven(): number | string {
-      return this.priceAveragePerBooking === 0 //prevents divide by 0 error
-        ? 'Cannot Determine without Average Price Per Booking'
-        : this.costOfDoingBusiness / this.priceAveragePerBooking
-    },
-    averageYearlyBookings(): number {
-      return this.bookingsPerMonth * 12
-    },
-    hoursWorkedYearly(): number {
-      return this.hoursAveragePerBooking * this.bookingsPerMonth * 12
-    },
-    averageHourlyRate(): any {
-      return this.hoursWorkedYearly === 0 //prevents divide by 0 error
-        ? 'Cannot Determine without Hours Worked Yearly'
-        : this.averageYearlyPay / this.hoursWorkedYearly
-    },
-    averageYearlyIncome(): number | string {
-      return this.hoursWorkedYearly === 0 //prevents divide by 0 error
-        ? 'Cannot Determine without Hours Worked Yearly'
-        : this.averageYearlyBookings * this.priceAveragePerBooking - this.costOfDoingBusiness
-    }
+    // averageYearly: (store: any) => {
+    //   console.log("in getter", store.averageYearly )
+    //   return  store.averageYearly * 12
+    // },
+    // averageYearlyPay(): number {
+    //   return this.payPerMonth * 12
+    // },
+    // averageYearlySavings(): number {
+    //   return this.savingsPerMonth * 12
+    // },
+    // averageYearlyFixedCosts(): number {
+    //   return this.totalFixedCosts * 12
+    // },
+    // averageYearlyVariableCosts(): number {
+    //   return this.totalVariableCosts * 12
+    // },
+    // costOfDoingBusiness(): number {
+    //   return (
+    //     this.averageYearly.averageYearlyFixedCosts +
+    //     this.averageYearlyVariableCosts +
+    //     this.averageYearlySavings +
+    //     this.averageYearlyPay
+    //   )
+    // },
+    // bookingsToBreakEven(): number | string {
+    //   return this.priceAveragePerBooking === 0 //prevents divide by 0 error
+    //     ? 'Cannot Determine without Average Price Per Booking'
+    //     : this.costOfDoingBusiness / this.priceAveragePerBooking
+    // },
+    // averageYearlyBookings(): number {
+    //   return this.bookingsPerMonth * 12
+    // },
+    // hoursWorkedYearly(): number {
+    //   return this.hoursAveragePerBooking * this.bookingsPerMonth * 12
+    // },
+    // averageHourlyRate(): any {
+    //   return this.hoursWorkedYearly === 0 //prevents divide by 0 error
+    //     ? 'Cannot Determine without Hours Worked Yearly'
+    //     : this.averageYearlyPay / this.hoursWorkedYearly
+    // },
+    // averageYearlyIncome(): number | string {
+    //   return this.hoursWorkedYearly === 0 //prevents divide by 0 error
+    //     ? 'Cannot Determine without Hours Worked Yearly'
+    //     : this.averageYearlyBookings * this.priceAveragePerBooking - this.costOfDoingBusiness
+    // },
   },
   actions: {
+    averageYearly(num: number) {
+      return num * 12
+    },
+    averageHourlyRate(hoursPerBooking: number, bookingsPerMonth: number, payPerMonth: number) {
+      // return this.hoursWorkedYearly === 0 //prevents divide by 0 error
+      return this.averageYearly(hoursPerBooking) * bookingsPerMonth === 0
+        ? 'Cannot Determine without Hours Worked Yearly'
+        : (this.averageYearly(payPerMonth) / this.averageYearly(hoursPerBooking)) *
+            this.bookingsPerMonth
+    },
+    costOfDoingBusiness(
+      totalFixedCosts: number,
+      totalVariableCosts: number,
+      savingsPerMonth: number,
+      payPerMonth: number
+    ) {
+      
+      return (
+        this.averageYearly(totalFixedCosts) +
+        this.averageYearly(totalVariableCosts) +
+        this.averageYearly(savingsPerMonth) +
+        this.averageYearly(payPerMonth)
+      )
+    },
+    averageYearlyIncome(
+      hoursPerBooking: number,
+      bookingsPerMonth: number,
+      payPerMonth: number,
+      priceAveragePerBooking: number,
+      totalFixedCosts: number,
+      totalVariableCosts: number,
+      savingsPerMonth: number,
+
+      
+    ) {
+      // return this.hoursWorkedYearly === 0 //prevents divide by 0 error
+      return this.averageYearly(hoursPerBooking) * bookingsPerMonth === 0
+        ? 'Cannot Determine without Hours Worked Yearly'
+        : this.averageYearly(bookingsPerMonth) * priceAveragePerBooking - this.costOfDoingBusiness(totalFixedCosts, totalVariableCosts, savingsPerMonth, payPerMonth)
+    },
     addCompanyNameAction(companyName: string) {
       this.companyName = companyName
     },
