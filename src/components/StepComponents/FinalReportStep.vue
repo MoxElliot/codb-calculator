11e7dfa6394e38614bba4f26035c1dcf77cbd94d
Elliot Erickson <elliot_erickson@protonmail.com>
2023-05-05 12:09:43 -0700

    Utilized storeToRefs in FinalReportStep

--- src/components/StepComponents/FinalReportStep.vue
+++ src/components/StepComponents/FinalReportStep.vue
@@ -1,63 +1,79 @@
 <script setup lang="ts">
 import { useReportStore } from '@/stores/reportStore'
+import { storeToRefs } from 'pinia'
 
 //should fixed costs be multiplied by 12 as they are not reoccuring??
 
 const reportStore = useReportStore()
+
+const {
+  companyName,
+  bookingsPerMonth,
+  hoursAveragePerBooking,
+  totalVariableCosts,
+  totalFixedCosts,
+  payPerMonth,
+  savingsPerMonth,
+} = storeToRefs(reportStore)
+
+const {
+  averageYearly,
+  costOfDoingBusiness,
+  bookingsToBreakEven,
+  averageMonthlyHourlyRate,
+  averageMonthlyIncome
+} = reportStore
 </script>
+
 <template>
   <div class="m-3">
     <h4>Final Report Step</h4>
   </div>
   <div class="m-3">
-    <p>Company Name: {{ reportStore.companyName }}</p>
+    <p>Company Name: {{ companyName }}</p>
   </div>
   <div class="m-3">
     <p>Your Yearly Variable Costs</p>
-    <p>{{ reportStore.averageYearly(reportStore.totalVariableCosts) }}</p>
+    <p>{{ averageYearly(totalVariableCosts) }}</p>
     <p>Your Yearly Fixed Costs</p>
-    <p>{{ reportStore.totalFixedCosts }}</p>
+    <p>{{ totalFixedCosts }}</p>
   </div>
   <div class="m-3">
     <div>
       <p>Your Yearly Savings</p>
-      <p>{{ reportStore.averageYearly(reportStore.savingsPerMonth) }}</p>
+      <p>{{ averageYearly(savingsPerMonth) }}</p>
     </div>
     <div>
       <p>Owners Draw (Yearly Salary)</p>
-      <p>{{ reportStore.averageYearly(reportStore.payPerMonth) }}</p>
+      <p>{{ averageYearly(payPerMonth) }}</p>
     </div>
     <div>
-      <p>{{ reportStore.companyName }}'s Cost of Doing Business'</p>
-      <p>${{ reportStore.costOfDoingBusiness }}</p>
+      <p>{{ companyName }}'s Cost of Doing Business'</p>
+      <p>${{ costOfDoingBusiness }}</p>
     </div>
   </div>
   <div class="m-3">
     <div>
       <p>Number of Yearly Bookings to Break Even</p>
-      <p>{{ reportStore.averageYearly(reportStore.bookingsToBreakEven) }}</p>
+      <p>{{ averageYearly(bookingsToBreakEven) }}</p>
     </div>
     <div>
       <p>Average Yearly Bookings</p>
-      <p>{{ reportStore.averageYearly(reportStore.bookingsPerMonth) }}</p>
+      <p>{{ averageYearly(bookingsPerMonth) }}</p>
     </div>
     <div>
       <p>Average Hours Worked Per Year</p>
       <p>
-        {{
-          reportStore.averageYearly(
-            reportStore.hoursAveragePerBooking * reportStore.bookingsPerMonth
-          )
-        }}
+        {{ averageYearly(hoursAveragePerBooking * bookingsPerMonth) }}
       </p>
     </div>
     <div>
       <p>Your Hourly Rate</p>
-      <p>${{ reportStore.averageYearly(reportStore.averageMonthlyHourlyRate) }}</p>
+      <p>${{ averageYearly(averageMonthlyHourlyRate) }}</p>
     </div>
     <div>
       <p>Your Average Yearly Income</p>
-      <p>${{ reportStore.averageYearly(reportStore.averageMonthlyIncome) }}</p>
+      <p>${{ averageYearly(averageMonthlyIncome) }}</p>
     </div>
   </div>
 </template>
