254e6cc48006effef442f7d00ac93dbc4699dbdd
Elliot Erickson <elliot_erickson@protonmail.com>
2023-05-22 15:45:07 -0700

    Forms validate on Fixed Cost Step, error message displays, need to validate on click not on blur

--- src/components/FixedCostsComponents/FixedCostDataInput.vue
+++ src/components/FixedCostsComponents/FixedCostDataInput.vue
@@ -12,16 +12,15 @@ import { storeToRefs } from 'pinia'
 
 const reportStore = useReportStore()
 
-const fixedCostName = ref<string>('')
-const fixedCostCategory = ref<string>('')
-const fixedCostAmount = ref<number>(0)
-const fixedCostPeriod = ref<string>('')
+// const fixedCostName = ref<string>('')
+// const fixedCostCategory = ref<string>('')
+// const fixedCostAmount = ref<number>(0)
+// const fixedCostPeriod = ref<string>('')
 const fixedCostTotal = ref<number>(0)
 
 const handleAddCost = (...values: any) => {
   console.log(JSON.stringify([...values], null, 2))
-  console.log('in handleAddCost', fixedCostName.value)
-  console.log('in handledAddCost allFilled validForm',allFilled)
+  // console.log('in handledAddCost allFilled validForm',allFilled)
   reportStore.addFixedCostAction({
     id: reportStore.fixedCosts.length + 1,
     name: fixedCostName.value,
@@ -37,57 +36,74 @@ const handleAddCost = (...values: any) => {
   fixedCostTotal.value = 0.0
 }
 
-const allFilled = {name: false, category: false, amount: false, period: false}
-const validForm = Object.values(allFilled).every(item => item === true)
+// const allFilled = {name: false, category: false, amount: false, period: false}
+// const validForm = Object.values(allFilled).every(item => item === true)
 
-const fixedCostNameInput: WritableComputedRef<string> = computed<string>({
-  get: () => fixedCostName.value, //Maintains data in field if user goes back
-  set: (text: string) => {
-    name.value = text
-    allFilled.name = true
-    // updateInputValidAction(true) // enables the Next button
-  }
-})
-const fixedCostCategoryInput: WritableComputedRef<string> = computed<string>({
-  get: () => fixedCostCategory.value, //Maintains data in field if user goes back
-  set: (text: string) => {
-    category.value = text
-    allFilled.category = true
-    // updateInputValidAction(true) // enables the Next button
-  }
-})
-const fixedCostAmountInput: WritableComputedRef<any> = computed<any>({
-  get: () => fixedCostAmount.value, //Maintains data in field if user goes back
-  set: (num: number) => {
-    amount.value = num
-    allFilled.amount = true
-    // updateInputValidAction(true) // enables the Next button
-  }
-})
-const fixedCostPeriodInput: WritableComputedRef<string> = computed<string>({
-  get: () => fixedCostPeriod.value, //Maintains data in field if user goes back
-  set: (text: string) => {
-    period.value = text
-    allFilled.period = true
-    // updateInputValidAction(true) // enables the Next button
-  }
-})
+// const fixedCostNameInput: WritableComputedRef<string> = computed<string>({
+//   get: () => fixedCostName.value, //Maintains data in field if user goes back
+//   set: (text: string) => {
+//     name.value = text
+//     allFilled.name = true
+//     // updateInputValidAction(true) // enables the Next button
+//   }
+// })
+// const fixedCostCategoryInput: WritableComputedRef<string> = computed<string>({
+//   get: () => fixedCostCategory.value, //Maintains data in field if user goes back
+//   set: (text: string) => {
+//     category.value = text
+//     allFilled.category = true
+//     // updateInputValidAction(true) // enables the Next button
+//   }
+// })
+// const fixedCostAmountInput: WritableComputedRef<any> = computed<any>({
+//   get: () => fixedCostAmount.value, //Maintains data in field if user goes back
+//   set: (num: number) => {
+//     amount.value = num
+//     allFilled.amount = true
+//     // updateInputValidAction(true) // enables the Next button
+//   }
+// })
+// const fixedCostPeriodInput: WritableComputedRef<string> = computed<string>({
+//   get: () => fixedCostPeriod.value, //Maintains data in field if user goes back
+//   set: (text: string) => {
+//     period.value = text
+//     allFilled.period = true
+//     // updateInputValidAction(true) // enables the Next button
+//   }
+// })
 
-const schema = Yup.object().shape({
+defineProps({
+  // Must be defined
+  modelValue: {
+    type: [String, Number]
+  },
+});
+
+const schema = Yup.object({
   name: Yup.string().required('Name is Required'),
   category: Yup.string().required('Category is Required'),
   amount: Yup.number().required('Value is Required'),
   period: Yup.string().required('Period is Required')
 })
 
-useForm({
+const { errors, values, meta } = useForm({
   validationSchema: schema,
 })
+// const [fixedCostName,fixedCostCategory, fixedCostAmount,fixedCostPeriod, ] = useFieldModel(['name', 'category', 'amount', 'period'])
 
-const { value: name, errorMessage: nameError } = useField('name')
-const { value: category, errorMessage: categoryError } = useField('category')
-const { value: amount, errorMessage: amountError } = useField('amount')
-const { value: period, errorMessage: periodError } = useField('period')
+
+const { value: fixedCostName, errorMessage: nameError, } = useField('name', values, {
+  initialValue: '',
+})
+const { value: fixedCostCategory, errorMessage: categoryError } = useField('category', values, {
+  initialValue: '',
+})
+const { value: fixedCostAmount, errorMessage: amountError } = useField('amount', values, {
+  initialValue: 0
+})
+const { value: fixedCostPeriod, errorMessage: periodError } = useField('period', values, {
+  initialValue: '',
+})
 </script>
 
 <template>
@@ -99,52 +115,54 @@ const { value: period, errorMessage: periodError } = useField('period')
     <fieldset class="fixed-cost-fieldset flex flex-row flex-1">
       <div>
         <data-input
-        v-model="fixedCostNameInput"
+        v-model="fixedCostName"
         label="Expense Name"
         type="input"
         id="expense-name"
         name="name"
         class="fixed-cost-dataset basis-1/4 flex-1"
       />
-      <span class="text-red font-semibold">{{ nameError }}</span>
+      <span class="text-red font-semibold">{{ errors.name }}</span> 
+        <!--ALSO v-models = fixedCostNameInput...-->
       </div>
       <div>
         <data-select
-        v-model="fixedCostCategoryInput"
+        v-model="fixedCostCategory"
         label="Category"
         name="category"
         :optionArray="costCategoryOptions"
         class="basis-1/4 flex-1"
       />
-      <span class="text-red font-semibold">{{ categoryError }}</span>
+      <span class="text-red font-semibold">{{ errors.category }}</span>
       </div>
       <div>
         <data-input
-        v-model="fixedCostAmountInput"
+        v-model="fixedCostAmount"
         label="Expense Amount"
         name="amount"
         class="fixed-cost-dataset basis-1/4 flex-1"
-        min="0"
-        step="0.01"
       />
       <span class="text-red font-semibold">{{ amountError }}</span>
       </div>
       <div>
         <data-select
-        v-model="fixedCostPeriodInput"
+        v-model="fixedCostPeriod"
         label="Pay Period"
         name="period"
         :optionArray="costPeriodOptions"
         class="basis-1/4 flex-1"
       />
-      <span class="text-red font-semibold">{{ periodError }}</span>
+      <span class="text-red font-semibold">{{ errors.period }}</span>
       </div>
-      
+     
     </fieldset>
+    
     <div class="btn-add flex flex-col justify-center">
       <form-button label="Add" type="submit" class="font-bold" />
     </div>
+    
   </Form>
+  <!-- <span class="text-red font-semibold">{{ errors }}</span> -->
 </template>
 
 <style scoped></style>
