c6c7d926dc652ead9daca0b98d97d1a19de8aa71
Elliot Erickson <elliot_erickson@protonmail.com>
2023-05-23 14:22:56 -0700

    Researching Provide/Inject next

--- src/components/StepComponents/CompanyNameStep.vue
+++ src/components/StepComponents/CompanyNameStep.vue
@@ -3,24 +3,32 @@ import { useReportStore } from '@/stores/reportStore'
 import * as Yup from 'yup'
 import DataInput from '../FormComponents/DataInput.vue'
 import { computed, onMounted, type WritableComputedRef } from 'vue'
-import { useForm, useField } from 'vee-validate'
+import { useForm, useField, Form } from 'vee-validate'
 import { storeToRefs } from 'pinia'
 
 const reportStore = useReportStore()
 const { addCompanyNameAction, updateInputValidAction } = reportStore
 const { companyName } = storeToRefs(reportStore)
 
-// onMounted(() => {
-//   updateInputValidAction(companyName.value === '' ? false : true )
-// })
+onMounted(() => {
+  console.log(
+    'on mount meta.valid, meta.touched, meta.intitalValue, meta.dirty',
+    meta.valid,
+    meta.touched,
+    meta.initialValue,
+    meta.dirty
+  )
+})
 
 const companyNameInput: WritableComputedRef<string> = computed<string>({
-  get: () => companyName.value,  //Maintains data in field if user goes back
-  set: async (text:string) => {
+  get: () => companyName.value, //Maintains data in field if user goes back
+  set: async (text: string) => {
     company.value = text
     const resp = await companyNameForm.validate()
-    addCompanyNameAction(text)    // updates Pinia state
-    updateInputValidAction(resp.valid)  // enables the Next button
+    addCompanyNameAction(text) // updates Pinia state
+    updateInputValidAction(resp.valid) // enables the Next button
+    console.log('in companyNameInput metaDirty meta.valid', meta.dirty, meta.valid)
+
   }
 })
 
@@ -30,20 +38,26 @@ const schema = Yup.object({
 
 const companyNameForm = useForm({
   validationSchema: schema,
+  validateOnMount: true,
+
 })
 
-const { value: company, errorMessage: companyError } = useField('company')
+const { value: company, errorMessage: companyError, meta } = useField('company')
+
 
 </script>
 
 <template>
-  <data-input
-    v-model.trim="companyNameInput"
-    name="company"
-    label="Company Name:"
-    type="text"
-    id="company-name"
-    
-  />
+  <Form >
+    <data-input
+      v-model.trim="companyNameInput"
+      name="company"
+      :meta="meta"
+      label="Company Name:"
+      type="text"
+      id="company-name"
+    />
+  </Form>
+
   <span class="text-red font-semibold">{{ companyError }}</span>
 </template>
--- src/components/views/Cobd.vue
+++ src/components/views/Cobd.vue
@@ -2,10 +2,17 @@
 import CodbForm from '../CoBDComponents/CodbForm.vue'
 import CodbHeader from '../CoBDComponents/CodbHeader.vue'
 import CodbFooter from '../CoBDComponents/CodbFooter.vue'
+import { Field, Form, useForm  } from 'vee-validate'
+
+const { meta } = useForm()
+
+console.log("in Cobd.vue meta", meta)
 </script>
 
 <template>
-  <codb-header />
-  <codb-form />
-  <codb-footer />
+  <Form >
+    <codb-header />
+    <codb-form />
+    <codb-footer />
+  </Form>
 </template>
