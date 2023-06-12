2061c658a7c29b81ff1200c4ade31e5ab790e526
Elliot Erickson <elliot_erickson@protonmail.com>
2023-06-12 15:52:41 -0700

    created Modal template and store

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
+<script setup lang="ts">
+import FormModal from '@/components/ModalComponents/FormModal.vue'
+</script>
 <template>
   <div class="h-screen">
+    <form-modal />
     <RouterView />
   </div>
 </template>
+
+
