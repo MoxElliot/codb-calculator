edbddfcad6a1dcd7dff06aeee0e4183fd14c04bf
Elliot Erickson <elliot_erickson@protonmail.com>
2023-06-16 14:48:57 -0700

    Background Image Options

---
+++ src/components/FormComponents/BackgroundImage.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+const props = defineProps({
+  tlImage: {
+    type: String,
+    default: ''
+  },
+  tlClass: {
+    type: String,
+    default: ''
+  },
+  trImage: {
+    type: String,
+    default: ''
+  },
+  trClass: {
+    type: String,
+    default: ''
+  },
+  brImage: {
+    type: String,
+    default: ''
+  },
+  brClass: {
+    type: String,
+    default: ''
+  },
+  blImage: {
+    type: String,
+    default: ''
+  },
+  blClass: {
+    type: String,
+    default: ''
+  },
+ 
+})
+</script>
+<template>
+    <img :src='tlImage' :class='tlClass' />
+    <img :src='trImage' :class='trClass' />
+    <img :src='blImage' :class='blClass' />
+    <img :src='brImage' :class='brClass' />
+</template>
--- src/components/StepComponents/Home.vue
+++ src/components/StepComponents/Home.vue
@@ -1,7 +1,11 @@
-git
 <script setup lang="ts">
 import { useReportStore } from '@/stores/reportStore'
 import { onMounted } from 'vue'
+import BackgroundImage from '../FormComponents/BackgroundImage.vue'
+import HomeBackgroundTL from '../../images/home_background_tl.svg'
+import HomeBackgroundTR from '../../images/home_background_tr.svg'
+import HomeBackgroundBL from '../../images/home_background_bl.svg'
+import HomeBackgroundBR from '../../images/home_background_br.svg'
 
 const reportStore = useReportStore()
 const { updateInputValidAction } = reportStore
@@ -12,14 +16,22 @@ onMounted(() => {
 </script>
 
 <template>
+  <BackgroundImage
+    :tl-image="HomeBackgroundTL" tl-class="absolute top-4% left-0 h-40/100"
+    :tr-image="HomeBackgroundTR" tr-class="absolute top-4% right-0 h-40/100"
+    :bl-image="HomeBackgroundBL" bl-class="absolute bottom-9% left-7% h-40/100"
+    :br-image="HomeBackgroundBR" br-class="absolute bottom-4% right-7% h-40/100"
+  />
+
+  <!-- <HomeBackground class="absolute top-0 left-0 bottom-0 right-0 bg-no-repeat bg-cover bg-center h-100 w-100"/> -->
   <!-- <div
     class="absolute top-0 left-0 bg-homeBackground bg-no-repeat bg-contain bg-center h-100 w-100"
   ></div> -->
   <!-- <div
-    class="absolute top-14% left-4% bg-homeBackgroundTL bg-no-repeat bg-contain bg-center h-35/100 w-35/100"
+    class="absolute top-4% left-4% bg-homeBackgroundTL bg-no-repeat bg-contain bg-center h-35/100 w-35/100"
   ></div>
   <div
-    class="absolute top-18% right-2% bg-homeBackgroundTR bg-no-repeat bg-contain bg-center h-4/10 w-25/100"
+    class="absolute top-9% right-2% bg-homeBackgroundTR bg-no-repeat bg-contain bg-center h-4/10 w-25/100"
   ></div>
   <div
     class="absolute bottom-9% left-7% bg-homeBackgroundBL bg-no-repeat bg-contain bg-center h-15/100 w-15/100"
@@ -27,13 +39,14 @@ onMounted(() => {
   <div
     class="absolute bottom-4% right-7% bg-homeBackgroundBR bg-no-repeat bg-contain bg-center h-25/100 w-25/100"
   ></div> -->
+
   <div class="flex flex-row justify-center basis-full">
     <div
       class="flex flex-col items-center justify-around lg:basis-2/3 sm:basis-3/4 basis-full px-8 overflow-auto"
     >
       <div class="flex">
         <p class="font-serif text-grey-300 sm:text-heading2 text-heading2_xs">Hey there!</p>
-        <img src="../../images/waving_hand.svg" alt="Waving Hand" width="40" height="40">
+        <img src="../../images/waving_hand.svg" alt="Waving Hand" width="40" height="40" />
       </div>
 
       <div class="font-sans text-grey-300 sm:text-body2 text-body2_xs text-center">
@@ -45,7 +58,13 @@ onMounted(() => {
       <div
         class="flex info-blue font-sans text-blue-300 sm:text-body text-body_xs m-6 sm:mt-2 md:py-6 md:pr-24 md:pl-16 sm:py-3 pr-12 pl-12"
       >
-        <img src="../../images/info-question-mark.svg" alt="Question" class="p-1 self-start" width="40" height="40"/>
+        <img
+          src="../../images/info-question-mark.svg"
+          alt="Question"
+          class="p-1 self-start"
+          width="40"
+          height="40"
+        />
         <div>
           <p>This is a self-guided excercise, so don't feel rushed!</p>
           <p>Using the NEXT STEP button, navigate through each section until the end!</p>
