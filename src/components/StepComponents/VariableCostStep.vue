ee9ad63822d714d8f234e775fa469899d2380001
Elliot Erickson <elliot_erickson@protonmail.com>
2023-06-18 14:08:40 -0700

    matched the VariableCostStep styling to FixedCosts

--- src/components/StepComponents/VariableCostStep.vue
+++ src/components/StepComponents/VariableCostStep.vue
@@ -4,6 +4,13 @@ import VariableCostDataInput from '../VariableCostsComponents/VariableCostDataIn
 import VariableCostTable from '../VariableCostsComponents/VariableCostTable.vue'
 import { useReportStore } from '@/stores/reportStore'
 import { storeToRefs } from 'pinia'
+import FormButton from '../FormComponents/FormButton.vue'
+import FormModal from '../ModalComponents/FormModal.vue'
+import { useModalStore } from '../../stores/modalStore'
+
+const modalStore = useModalStore()
+const { isOpen } = storeToRefs(modalStore)
+const { openModal } = modalStore
 
 const reportStore = useReportStore()
 const { updateInputValidAction } = reportStore
@@ -17,8 +24,55 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="basis-full flex flex-col justify-center items-center">
-    <variable-cost-data-input />
+
+<form-modal v-if="isOpen" class="flex flex-col">
+    <template #header>
+      <div
+        class="flex flex-row justify-center items-center text-heading2_xs md:text-heading text-grey-300 font-serif"
+      >
+        <p>Add Fixed Cost</p>
+      </div>
+    </template>
+    <template #body>
+      <variable-cost-data-input />
+    </template>
+  </form-modal>
+
+
+  <div class="flex flex-col text-center basis-full items-center justify-center">
+    <div
+      class="flex flex-row justify-center items-center text-heading2_xs md:text-heading text-grey-300 font-serif md:basis-1/6 md:w-6/10 md:mb-8"
+    >
+      <p class="">
+        Variable Costs for <span>{{ companyName }}</span>
+      </p>
+      <img
+        src="../../images/fixed-cost-calandar.svg"
+        alt="Calandar"
+        class="hidden sm:inline sm:px-3"
+      />
+    </div>
+    <div class="text-body text-grey-300 basis-1/6 w-8/10 md:w-6/10">
+      <p>
+        A Variable Cost is any expense category that your are expecting to pay on an irregular basis!Add the
+        name of the cost, the category, the amount below.
+      </p>
+    </div>
+    <div class="basis-full flex flex-col items-start justify-center w-8/10 sm:w-7/10">
+      <div class="hidden md:flex">
+    <variable-cost-data-input  class="flex flex-row" />
+  </div>
+  <div class="md:hidden">
+        <div class="btn-add flex flex-col justify-center">
+          <form-button
+            label="+ Add Variable Cost"
+            type="submit"
+            class="font-bold"
+            @click="openModal()"
+          />
+        </div>
+      </div>
     <variable-cost-table />
+    </div>
   </div>
 </template>
