0c3f3428f1057fdf95eb20bc13392421f2a54dd4
Elliot Erickson <elliot_erickson@protonmail.com>
2023-06-19 21:16:21 -0700

    Added styling to final report

--- src/components/StepComponents/FinalReportStep.vue
+++ src/components/StepComponents/FinalReportStep.vue
@@ -100,15 +100,36 @@ const reportOutputsArray: { id: string; class: string; label: string; variable: 
 </script>
 
 <template>
-  <div class="basis-full flex flex-col justify-center items-center">
-    <div class="text-caption-bold">
-      <p>Final Report</p>
+  <div class="flex flex-col basis-full justify-center items-center text-center">
+    <div
+      class="flex flex-row justify-center items-center text-heading2_xs md:text-heading text-grey-300 font-serif md:basis-1/6 md:w-6/10 md:mb-8"
+    >
+      <p class="">
+        Final CoBD Report For <span>{{ companyName }}</span>
+      </p>
+      <img
+        src="../../images/fixed-cost-calandar.svg"
+        alt="Calandar"
+        class="hidden sm:inline sm:px-3"
+      />
     </div>
-    <div class="overflow-auto">
-      <report-output v-for="output in reportOutputsArray" :class="output.class" :key="output.id">
-        <template #top> {{ output.label }}</template>
-        <template #bottom> {{ output.variable }}</template>
-      </report-output>
+    <div class="text-grey-300 text-body2_xs md:text-body2 basis-1/6 w-8/10 md:w-6/10">
+      <p>You did it! Your CobD Calculation is Complete!</p>
+      <p>See the final report below!</p>
+    </div>
+    <div class="basis-full flex flex-col items-start justify-center w-6/10 sm:w-5/10">
+      <div class="font-sans text-grey-300 text-body mt-2 md:mt-6 px-6">
+        <div class="overflow-auto">
+          <report-output
+            v-for="output in reportOutputsArray"
+            :class="output.class"
+            :key="output.id"
+          >
+            <template #top> {{ output.label }}</template>
+            <template #bottom> {{ output.variable }}</template>
+          </report-output>
+        </div>
+      </div>
     </div>
   </div>
 </template>
