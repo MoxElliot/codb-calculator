60ff038f14963102ae1a54dd46387e94bf746437
Elliot Erickson <elliot_erickson@protonmail.com>
2023-06-20 20:26:09 -0700

    adjusted margins on medium screen + for home
    from justify-around to mb-10

--- src/components/StepComponents/Home.vue
+++ src/components/StepComponents/Home.vue
@@ -30,21 +30,21 @@ onMounted(() => {
   <!-- <HomeBackground class="invisible absolute top-0 left-0 bottom-0 right-0 bg-no-repeat bg-cover bg-center h-100 w-100"/> -->
 
   <div
-    class="flex flex-col items-center justify-around basis-full md:basis-3/4 lg:basis-2/3 px-8 overflow-auto"
+    class="flex flex-col items-center justify-between md:justify-center basis-full md:basis-3/4 lg:basis-2/3 px-8 overflow-auto"
   >
-    <div class="flex">
+    <div class="flex md:mb-10">
       <p class="font-serif text-grey-300 text-heading2_xs md:text-heading2">Hey there!</p>
       <img src="../../images/waving_hand.svg" alt="Waving Hand" width="40" height="40" />
     </div>
 
-    <div class="font-sans text-grey-300 md:text-body2 text-body2_xs text-center">
+    <div class="font-sans text-grey-300 md:text-body2 text-body2_xs text-center md:mb-10">
       <p class="">
         Welcome to the Cost of Doing Business (CoDB) tool. With this spreadsheet, you'll be able to
         easily calculate how much it costs for you to run your business!
       </p>
     </div>
     <div
-      class="flex info-blue font-sans text-blue-300 text-body_xs md:text-body my-2 md:m-6 md:py-6 md:pr-24 md:pl-16 p-3"
+      class="flex info-blue font-sans text-blue-300 text-body_xs md:text-body my-2 md:mb-10 md:py-6 md:pr-24 md:pl-16 p-3"
     >
       <img
         src="../../images/info-question-mark.svg"
