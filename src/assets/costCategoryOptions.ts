7c733f17bb7471a70cc47e9ecaf274959a2ee0d1
Elliot Erickson <elliot_erickson@protonmail.com>
2023-07-10 09:35:34 -0700

    added category images, final dropdown styling

--- src/assets/costCategoryOptions.ts
+++ src/assets/costCategoryOptions.ts
@@ -4,29 +4,29 @@ const costCategoryOptions: {
 }[] = [
   {
     category: 'Gallery/Image Delivery',
-    image: 'src/images/CostCategoryImages/client-managment-category.svg'
+    image: 'src/images/CostCategoryImages/gallery-image-delivery-category.svg'
   },
   {
     category: 'Website/Hosting',
-    image: 'src/images/CostCategoryImages/client-managment-category.svg'
+    image: 'src/images/CostCategoryImages/website-hosting-category.svg'
   },
-  { category: 'Insurance', image: 'src/images/CostCategoryImages/client-managment-category.svg' },
+  { category: 'Insurance', image: 'src/images/CostCategoryImages/insurance-category.svg' },
   {
     category: 'Email Service',
-    image: 'src/images/CostCategoryImages/client-managment-category.svg'
+    image: 'src/images/CostCategoryImages/email-category.svg'
   },
   {
     category: 'Client Management',
     image: 'src/images/CostCategoryImages/client-managment-category.svg'
   },
-  { category: 'Studio Rent', image: 'src/images/CostCategoryImages/client-managment-category.svg' },
+  { category: 'Studio Rent', image: 'src/images/CostCategoryImages/studio-rent-category.svg' },
   {
     category: 'Courses/Educaiton',
-    image: 'src/images/CostCategoryImages/client-managment-category.svg'
+    image: 'src/images/CostCategoryImages/courses-education-category.svg'
   },
   {
     category: 'Editing/Design Software',
-    image: 'src/images/CostCategoryImages/client-managment-category.svg'
+    image: 'src/images/CostCategoryImages/editing-design-software-category.svg'
   },
   { category: 'Other', image: 'src/images/CostCategoryImages/client-managment-category.svg' }
 ]
