b32baf4594a54882230b8e53ba59b3c1fed710ad
Elliot Erickson <elliot_erickson@protonmail.com>
2023-07-13 11:20:32 -0600

    added icons from home page

--- src/components/StepComponents/Home.vue
+++ src/components/StepComponents/Home.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { useReportStore } from '@/stores/reportStore'
 import { onMounted } from 'vue'
+import HomeInfoQuestionMark from '../IconComponents/HomeInfoQuestionMark.vue'
+import HomeWavingHand from '../IconComponents/HomeWavingHand.vue'
 import HomeBackground from '../../images/home_background.svg'
 
 const reportStore = useReportStore()
@@ -12,37 +14,30 @@ onMounted(() => {
 </script>
 
 <template>
-
   <img
     :src="HomeBackground"
     alt=""
-    class="hidden lg:block absolute top-0 left-0 bottom-0 right-0 h-100 w-100 z-10"
+    class="absolute bottom-0 left-0 right-0 top-0 z-10 hidden h-100 w-100 lg:block"
   />
-  <div class="flex basis-full justify-center items-center h-full z-20">
+  <div class="z-20 flex h-full basis-full items-center justify-center">
     <div
-      class="flex flex-col items-center justify-center basis-full md:basis-3/4 lg:basis-2/3 px-8"
+      class="flex basis-full flex-col items-center justify-center px-8 md:basis-3/4 lg:basis-2/3"
     >
-      <div class="flex mb-2 md:mb-10">
-        <p class="font-serif text-grey-300 text-heading2_xs md:text-heading2">Hey there!</p>
-        <img src="../../images/waving_hand.svg" alt="Waving Hand" width="40" height="40" />
+      <div class="mb-2 flex md:mb-10">
+        <p class="font-serif text-heading2_xs text-grey-300 md:text-heading2">Hey there!</p>
+        <home-waving-hand class="icon-sizing self-center ml-1" />
       </div>
 
-      <div class="font-sans text-grey-300 md:text-body2 text-body2_xs text-center mb-2 md:mb-10">
+      <div class="mb-2 text-center font-sans text-body2_xs text-grey-300 md:mb-10 md:text-body2">
         <p class="">
           Welcome to the Cost of Doing Business (CoDB) tool. With this spreadsheet, you'll be able
           to easily calculate how much it costs for you to run your business!
         </p>
       </div>
       <div
-        class="flex info-blue font-sans text-blue-300 text-body_xs md:text-body my-2 md:mb-10 md:py-6 md:pr-24 md:pl-16 p-3"
+        class="info-blue my-2 flex p-3 font-sans text-body_xs text-blue-300 md:mb-10 md:py-6 md:pl-16 md:pr-24 md:text-body"
       >
-        <img
-          src="../../images/info-question-mark.svg"
-          alt="Question"
-          class="p-1 self-start"
-          width="40"
-          height="40"
-        />
+        <home-info-question-mark class="icon-sizing self-start mr-2" />
         <div>
           <p>This is a self-guided excercise, so don't feel rushed!</p>
           <p>Using the NEXT STEP button, navigate through each section until the end!</p>
