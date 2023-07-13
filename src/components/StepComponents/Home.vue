4c9b0dc9162a847c81feef91f076200a20927201
Elliot Erickson <elliot_erickson@protonmail.com>
2023-07-13 14:10:28 -0600

    Home backgroud image is now an icon

--- src/components/StepComponents/Home.vue
+++ src/components/StepComponents/Home.vue
@@ -3,7 +3,7 @@ import { useReportStore } from '@/stores/reportStore'
 import { onMounted } from 'vue'
 import HomeInfoQuestionMark from '../IconComponents/HomeInfoQuestionMark.vue'
 import HomeWavingHand from '../IconComponents/HomeWavingHand.vue'
-import HomeBackground from '../../images/home_background.svg'
+import HomeBackground from '../IconComponents/HomBackground.vue'
 
 const reportStore = useReportStore()
 const { updateInputValidAction } = reportStore
@@ -14,9 +14,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <img
-    :src="HomeBackground"
-    alt=""
+  <home-background
     class="absolute bottom-0 left-0 right-0 top-0 z-10 hidden h-100 w-100 lg:block"
   />
   <div class="z-20 flex h-full basis-full items-center justify-center">
@@ -25,7 +23,7 @@ onMounted(() => {
     >
       <div class="mb-2 flex md:mb-10">
         <p class="font-serif text-heading2_xs text-grey-300 md:text-heading2">Hey there!</p>
-        <home-waving-hand class="icon-sizing self-center ml-1" />
+        <home-waving-hand class="icon-sizing ml-1 self-center" />
       </div>
 
       <div class="mb-2 text-center font-sans text-body2_xs text-grey-300 md:mb-10 md:text-body2">
@@ -37,7 +35,7 @@ onMounted(() => {
       <div
         class="info-blue my-2 flex p-3 font-sans text-body_xs text-blue-300 md:mb-10 md:py-6 md:pl-16 md:pr-24 md:text-body"
       >
-        <home-info-question-mark class="icon-sizing self-start mr-2" />
+        <home-info-question-mark class="icon-sizing mr-2 self-start" />
         <div>
           <p>This is a self-guided excercise, so don't feel rushed!</p>
           <p>Using the NEXT STEP button, navigate through each section until the end!</p>
